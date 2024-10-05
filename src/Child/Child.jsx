068670eef77f6bbfcd624e289cb02cfbe5ee6c87
ahmed mahmoud <ahmad.mahmoud1472003@gmail.com>
2024-10-05 17:04:42 +0300

    first commit

---
+++ src/Child/Child.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Child(props) {
+  //   let { fNamee, salary, age } = props.userDetailes;
+  let { id, fName, count, price, category, onSale } = props.products;
+
+
+  return (
+    <>
+      {/* <div>
+        Child age is {props.y} and count is {props.x}
+      </div>
+      <div>
+        Child age is {age} and user name is {fNamee} salary {salary}
+      </div> */}
+      <div className="w-1/4 p-2">
+        <div className="bg-slate-400 rounded-lg shadow-lg p-4 relative">
+          <h4>name is : {fName}</h4>
+          <h4>count is : {count}</h4>
+          <h4>price is : {price}</h4>
+          <h4>category is : {category}</h4>
+          {/* {onSale ? <div className=" absolute top-0 right-2 bg-red-600 p-2 text-white rounded-md">sale</div>: null }  or ===>*/} 
+          { onSale && <div className=" absolute top-0 right-2 bg-red-600 p-2 text-white rounded-md">sale</div> }
+          <button onClick={()=>props.delete(id)} className="bg-red-700 my-2 w-full p-2 text-white rounded-lg">Delete</button>
+          <button onClick={()=>props.update(props.index)} className="bg-yellow-500 my-2 w-full p-2 text-white rounded-lg">Update</button>
+
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./navBar";
 
 export default function Home() {
-    return (
-        //  <React.Fragment></React.Fragment>
-        <>  
-      <h2>home</h2>
+  let [count, setCount] = useState(0);
+  let [friends, setFriends] = useState(["ahmed", "ali"]);
+  let [user, setUser] = useState({ fName: "bog", age: 20 });
+  let [username, setUserName] = useState("mody");
+  function changeCount() {
+    setCount(Math.round(Math.random() * 100));
+  }
+
+  return (
+    //  <React.Fragment></React.Fragment>
+    <>
+      {/* binding { code js }  ===> wiriting code js inside jsx */}
+
+      <h2 className= {count > 50 ? "bg-green-600" : "bg-red-600" }>Count  {count}</h2>
+      <h3>the first friend is : {friends[0]}</h3>
+      <h4>user {user.fName} </h4>
+      <button
+        onClick={ () => changeCount()}
+        className="px-3 py-2 my-2 bg-slate-400 rounded-lg shadow-lg"
+      >
+        {" "}
+        click
+      </button>
       <NavBar />
     </>
   );
